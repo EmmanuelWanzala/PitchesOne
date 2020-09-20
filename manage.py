28e798588bf030e89801972c8acc23fc40db1942
EmmanuelWanzala <wanzalaemmanuel28@gmail.com>
2020-09-20 16:33:35 +0300

    add manager.shell and manager.py

--- manage.py
+++ manage.py
@@ -16,11 +16,11 @@ manager.add_command('run',Server(use_debugger=True))
 def make_shell_context():
     return dict(app = app,db = db,User = User)
 
-@manager.command
-def test():
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
+# @manager.command
+# def test():
+#     import unittest
+#     tests = unittest.TestLoader().discover('tests')
+#     unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == "__main__":
     manager.run()
