1ff39a968eafef4c965a1e1d6abd267e9aa88e6e
EmmanuelWanzala <wanzalaemmanuel28@gmail.com>
2020-09-20 16:14:21 +0300

    add all the classes and functions

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
-from . import db,login_manager
-from datetime import datetime
-from flask_login import UserMixin,current_user
-from werkzeug.security import generate_password_hash,check_password_hash
+# from . import db,login_manager
+# from datetime import datetime
+# from flask_login import UserMixin,current_user
+# from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
