28e37440346fb1ee1bddbef642e940bdd87f3dc1
EmmanuelWanzala <wanzalaemmanuel28@gmail.com>
2020-09-23 08:24:13 +0300

    reput sever and deleted start.sh

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS=True
     SECRET_KEY=os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
@@ -13,16 +13,18 @@ class Config:
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2-binary://moringa:Stress@localhost/pitchesone'
+
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
 
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Stress@localhost/pitchesone_test'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2-binary://moringa:Stress@localhost/pitchesone_test'
 
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Stress@localhost/pitchesone'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2-binary://moringa:Stress@localhost/pitchesone'
 
     DEBUG=True
 
